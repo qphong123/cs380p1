7df5def6f4deb1c6c43be5e86253b7b77a1ca49c
qphong123 <32345377+qphong123@users.noreply.github.com>
2017-10-06 22:53:32 -0700

    Add files via upload

---
+++ ChatClient.java
@@ -0,0 +1,42 @@
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.IOException;
+
+public class ChatClient{
+
+	public static void main(String[] args) throws IOException{
+		try(Socket socket = new Socket("18.221.102.182", 38001)){
+			Scanner kb = new Scanner(System.in);
+			InputStream in = socket.getInputStream();
+			InputStreamReader inR = new InputStreamReader(in, "UTF-8");
+			BufferedReader br = new BufferedReader(inR);
+			OutputStream os = socket.getOutputStream();
+			PrintStream out = new PrintStream(os);
+			System.out.print("Enter user name: ");
+			String username = kb.nextLine();
+			out.println(username);
+
+			Runnable receiver  = () -> {
+			   try{
+				String message;
+				while((message = br.readLine())!=null){
+					System.out.println(message);
+				}
+				System.exit(0);
+			   }catch (IOException e){
+				System.out.println("Exception thrown.");
+			   }
+			};
+			new Thread(receiver).start();
+			while(true){
+				out.println(kb.nextLine());
+			}
+		}
+	}
+
+}
